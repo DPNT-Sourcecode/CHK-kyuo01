48a2b4ed17ff10344edb06f501132d0ba0816986
sbx_user1051 <sbx_user1051@169.254.17.5>
2020-03-08 13:17:01 +0000

    Sun Mar 08 13:17:01 UTC 2020

--- src/main/java/befaster/solutions/HLO/HelloSolution.java
+++ src/main/java/befaster/solutions/HLO/HelloSolution.java
@@ -2,9 +2,19 @@ package befaster.solutions.HLO;
 
 public class HelloSolution {
     public String hello(String friendName) {
-        return "Hello, World!";
+    	if (friendName.equals("")) {
+    		return "Hello, World!";
+    	} 
+    	
+    	StringBuilder builder = new StringBuilder();
+    	builder.append("Hello, ") 
+    		   .append(friendName)
+    		   .append("!");
+    	
+    	return builder.toString();
     }
 }
+
 
 
 
