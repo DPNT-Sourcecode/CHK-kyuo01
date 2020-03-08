4f63da3fea9159a686e41a32b9dc340f24d37c11
sbx_user1051 <sbx_user1051@169.254.17.5>
2020-03-08 13:09:26 +0000

    Sun Mar 08 13:09:26 UTC 2020

--- src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
+++ src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
@@ -1,5 +1,16 @@
 package befaster.solutions.HLO;
 
-public class HelloSolutionTest {
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.text.IsEqualIgnoringCase.equalToIgnoringCase;
 
+import org.junit.Test;
+
+public class HelloSolutionTest {
+	HelloSolution solution = new HelloSolution();
+	
+	@Test
+	public void assertThatHelloReturnsWorld() {
+		assertThat(solution.hello(""), equalToIgnoringCase("world"));
+	}
 }
+
