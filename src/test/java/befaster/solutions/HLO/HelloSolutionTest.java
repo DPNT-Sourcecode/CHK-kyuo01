19c8773cc7f88097648a16469c747370022f191e
sbx_user1051 <sbx_user1051@169.254.17.5>
2020-03-08 13:15:51 +0000

    Sun Mar 08 13:15:51 UTC 2020

--- src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
+++ src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
@@ -9,9 +9,15 @@ public class HelloSolutionTest {
 	HelloSolution solution = new HelloSolution();
 	
 	@Test
-	public void assertThatHelloReturnsWorld() {
+	public void assertThatHelloWithNoParamReturnsHelloWorld() {
 		assertThat(solution.hello(""), equalTo("Hello, World!"));
 	}
+	
+	@Test
+	public void assertThatHelloWithParamJohnReturnsHelloJohn() {
+		assertThat(solution.hello("John"), equalTo("Hello, John!"));
+	}
 }
+
 
 
