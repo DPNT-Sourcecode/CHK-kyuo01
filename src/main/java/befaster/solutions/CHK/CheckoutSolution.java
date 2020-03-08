e477172002b0d436ab20d6fb0922587482e27b93
sbx_user1051 <sbx_user1051@169.254.17.5>
2020-03-08 13:40:26 +0000

    Sun Mar 08 13:40:26 UTC 2020

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,11 +1,41 @@
 package befaster.solutions.CHK;
 
-import befaster.runner.SolutionNotImplementedException;
-
 public class CheckoutSolution {
     public Integer checkout(String skus) {
-        throw new SolutionNotImplementedException();
+    	int countA=0, countB=0; // counters for discount items
+    	int total=0;
+    	
+    	for (int i=0; i<skus.length(); i++) {
+    		switch (skus.charAt(i)) {
+    		case ('A'):
+    			countA++;
+    			total+=50;
+    			break;
+    		case ('B'):
+    			total+=30;
+    			countB++;
+    			break;
+    		case ('C'):
+    			total+=20;
+    			break;
+    		case ('D'):
+    			total+=15;
+    			break;
+    		default:
+    			return -1;
+    		}
+    	}
+    	
+    	if (countA>=3) {
+    		total-=20;
+    	}
+    	if (countB>=2) {
+    		total-=15;
+    	}
+    	
+        return total;
     }
 }
+
 
 
