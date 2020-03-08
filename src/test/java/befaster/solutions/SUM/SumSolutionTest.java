28af91f855db41fe8636e75daf7ef9fe0d7b7b84
sbx_user1051 <sbx_user1051@169.254.17.5>
2020-03-08 12:53:54 +0000

    Sun Mar 08 12:53:54 UTC 2020

--- src/test/java/befaster/solutions/SUM/SumSolutionTest.java
+++ src/test/java/befaster/solutions/SUM/SumSolutionTest.java
@@ -1,13 +1,18 @@
 package befaster.solutions.SUM;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 public class SumSolutionTest {
     private SumSolution sum;
+    
+    @Rule
+    ExpectedException thrown = ExpectedException.none();
 
     @Before
     public void setUp() {
@@ -19,4 +24,39 @@ public class SumSolutionTest {
     public void compute_sum() {
         assertThat(sum.compute(1, 1), equalTo(2));
     }
+    
+    @Test
+    public void assertThatLowestSumIsZero() {
+    	assertThat(sum.compute(0, 0), equalTo(0));
+    }
+    
+    @Test
+    public void assertThatGreatestSumIsTwoHundred() {
+    	assertThat(sum.compute(100, 100), equalTo(200));
+    }
+    
+    @Test
+    public void assertThatParam1LessThanZeroThrowsException() {
+    	thrown.expect(ParameterOutOfBoundsException.class);
+    	sum.compute(-1,0);
+    }
+    
+    @Test
+    public void assertThatParam2LessThanZeroThrowsException() {
+    	thrown.expect(ParameterOutOfBoundsException.class);
+    	sum.compute(0,-1);
+    }
+    
+    @Test
+    public void assertThatParam1GreaterThan100ThrowsException() {
+    	thrown.expect(ParameterOutOfBoundsException.class);
+    	sum.compute(101, 0);
+    }
+    
+    @Test
+    public void assertThatParam2GreaterThan100ThrowsException() {
+    	thrown.expect(ParameterOutOfBoundsException.class);
+    	sum.compute(0,101);
+    }
 }
+
